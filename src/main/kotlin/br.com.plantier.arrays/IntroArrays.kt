9eb0d6b32df973ef1ab19700eb849f323e61a7bc
Junior Plantier <junior.plantier@gmail.com>
2023-04-07 22:36:46 -0300

    Introdução Arrays

--- src/main/kotlin/br.com.plantier.arrays/IntroArrays.kt
+++ src/main/kotlin/br.com.plantier.arrays/IntroArrays.kt
@@ -1,5 +1,21 @@
 package br.com.plantier.arrays
 
 fun main() {
+    val idades: IntArray = intArrayOf(18, 34, 22, 32, 38, 41)
 
+    var maiorIdade = Int.MIN_VALUE
+    for(idade in idades) {
+        if(idade > maiorIdade) {
+            maiorIdade = idade
+        }
+    }
+    println(maiorIdade)
+
+    var menorIdade = Int.MAX_VALUE
+    idades.forEach { idade ->
+        if(menorIdade > idade) {
+            menorIdade = idade
+        }
+    }
+    println(menorIdade)
 }
