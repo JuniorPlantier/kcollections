eaa55bbf763b32063a5e582eeb74f5cb0f9f47ed
Junior Plantier <junior.plantier@gmail.com>
2023-04-27 14:36:04 -0300

    testando o Set

--- src/main/kotlin/br/com/plantier/collections/main.kt
+++ src/main/kotlin/br/com/plantier/collections/main.kt
@@ -1,5 +1,15 @@
 package br.com.plantier.collections
 
 fun main() {
-    val assistiramCursoAndroid = listOf("Lina", "Fran", "Valdo", "Math")
+    val assistiramCursoAndroid: MutableSet<String> = mutableSetOf("Lina", "Fran", "Valdo", "Carlos")
+    val assistiramCursoKotlin = mutableSetOf("Lina", "Lari", "Joao")
+    val assistiramAmbos = mutableSetOf<String>()
+
+    assistiramAmbos.addAll(assistiramCursoAndroid)
+    assistiramAmbos.addAll(assistiramCursoKotlin)
+
+    assistiramAmbos.add("Ana")
+    assistiramAmbos.add("Ana")
+
+    println(assistiramAmbos)
 }
