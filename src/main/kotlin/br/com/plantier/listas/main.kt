1c388283af0f413a49fc56921413b46967e3c83a
Junior Plantier <junior.plantier@gmail.com>
2023-04-27 00:34:40 -0300

    multableList

--- src/main/kotlin/br/com/plantier/listas/main.kt
+++ src/main/kotlin/br/com/plantier/listas/main.kt
@@ -1,12 +1,31 @@
 package br.com.plantier.listas
 
 fun main() {
+    val banco = BancoDeDados()
+//    banco.nomes.add("Lina")
+    banco.salvar("Lina")
+
+    println(banco.nomes)
+}
+
+class BancoDeDados {
+    // a gente não pode fazer com que qualquer um consiga manipular nossa coleção
+    private val dados = mutableListOf<String>()
+
+    val nomes: Collection<String> get() = dados
+
+    fun salvar(nome: String) {
+        dados.add(nome)
+    }
+}
+
+fun testaColecao() {
     // Iterable > Collection > List
-    val nomes: Collection<String> = listOf("Lina", "Lari", "Carlao", "Valdir", "Fatima", "Vera Lucia")
+    val nomes: List<String> = listOf("Lina", "Lari", "Carlao", "Valdir", "Fatima", "Vera Lucia")
 
     for(nome in nomes) {
         println(nome)
     }
     println("Tem o nome Lina? ${nomes.contains("Lina")}")
-    println("Tamanho da coleção: ${nomes.size()}")
+    println("Tamanho da coleção: ${nomes.size}")
 }
